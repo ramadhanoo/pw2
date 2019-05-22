48c04e1df3ca85ad5b9af149044320e6f3c89d9c
rahmat <rahmat@genggamgemilang.com>
2019-05-22 10:17:51 +0700

    2019-05-22

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -16,7 +16,7 @@ class Upload extends CI_Controller {
         public function do_upload()
         {
                 $config['upload_path']          = './uploads/';
-                $config['allowed_types']        = 'gif|jpg|png|jpeg';
+                $config['allowed_types']        = 'gif|jpg|png';
                 $config['max_size']             = 100;
                 $config['max_width']            = 1024;
                 $config['max_height']           = 768;
